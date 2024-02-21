56f00227f729715879c3da662067dee52aa8aeba
Dimitar <dimitar.mandarliev@gmail.com>
2024-02-21 13:10:55 +0200

    Single User and Product Page Design

---
+++ app/dashboard/users/[id]/page.jsx
@@ -0,0 +1,43 @@
+import styles from "@/app/ui/dashboard/users/singleUser/singleUser.module.css";
+import Image from "next/image";
+
+function SingeUserPage() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.infoContainer}>
+        <div className={styles.imgContainer}>
+          <Image src="/noavatar.png" alt="" fill />
+        </div>
+        John Doe
+      </div>
+      <div className={styles.formContainer}>
+        <form className={styles.form}>
+          <label>Username</label>
+          <input type="text" name="username" placeholder="John Doe" />
+          <label>Email</label>
+          <input type="email" name="email" placeholder="john@gmail.com" />
+          <label>Password</label>
+          <input type="password" name="password" placeholder="Password" />
+          <label>Phone</label>
+          <input type="text" name="phone" placeholder="+1234567" />
+          <label>Address</label>
+          <textarea type="text" name="address" placeholder="New York" />
+          <label>Is Admin?</label>
+          <select name="isAdmin" id="isAdmin">
+            <option value={true}>Yes</option>
+            <option value={false}>No</option>
+          </select>
+          <label>Is Active?</label>
+          <select name="isActive" id="isActive">
+            <option value={true}>Yes</option>
+            <option value={false}>No</option>
+          </select>
+
+          <button>Update</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default SingeUserPage;
